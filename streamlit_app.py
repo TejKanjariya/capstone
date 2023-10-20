a6ceeee219d0320d67a69fb17e33afa72782a760
Tej kanjariya <73520835+TejKanjariya@users.noreply.github.com>
2023-10-20 23:47:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,6 +114,7 @@ input42 = st.selectbox("Select Current Ball", min_value=0, max_value=10, value=0
 # input9 = st.number_input("Input 9", value=0.0)
 # input10 = st.slider("Input 10", min_value=0, max_value=10, value=5)
 neural_input = [input41, input42, player_data_dict[input4][1], player_data_dict[input4][2], player_data_dict[input4][0]]
+print("input here :-", neural_input)
 if st.button("Predict"):
     # Map selected values to their respective integer values
     # input1 = batting_team[input1]
