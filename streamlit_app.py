42153d57058f6f6ccd6b9793d5cb3caf58f03ecf
Tej kanjariya <73520835+TejKanjariya@users.noreply.github.com>
2023-10-20 18:55:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import pickle
 import sklearn
+import pickle
+
 
 # Your model loading code (e.g., using joblib, pickle, or your preferred method)
 # Load the trained model from the pickle file
