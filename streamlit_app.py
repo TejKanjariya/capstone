2dfba7fd1f7bd834f6204d7d144747c18261af66
Tej kanjariya <73520835+TejKanjariya@users.noreply.github.com>
2023-10-20 22:51:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,14 @@ model_filename = 'random_forest_model.pkl'
 with open(model_filename, 'rb') as model_file:
     model = pickle.load(model_file)
 
+batting_team =
+{'Royal Challengers Bangalore': 0, 'Rising Pune Supergiant': 1, 'Kolkata Knight Riders': 2, 'Kings XI Punjab': 3, 'Delhi Daredevils': 4, 'Sunrisers Hyderabad': 5, 'Mumbai Indians': 6, 'Gujarat Lions': 7, 'Chennai Super Kings': 8, 'Rajasthan Royals': 9, 'Delhi Capitals': 10, 'Deccan Chargers': 11}
+
+bowling_team =
+{'Sunrisers Hyderabad': 0, 'Mumbai Indians': 1, 'Gujarat Lions': 2, 'Rising Pune Supergiant': 3, 'Royal Challengers Bangalore': 4, 'Kolkata Knight Riders': 5, 'Delhi Daredevils': 6, 'Kings XI Punjab': 7, 'Rajasthan Royals': 8, 'Chennai Super Kings': 9, 'Delhi Capitals': 10, 'Deccan Chargers': 11}
+
+city =
+{'Hyderabad': 0, 'Pune': 1, 'Rajkot': 2, 'Indore': 3, 'Bengaluru': 4, 'Mumbai': 5, 'Kolkata': 6, 'Bangalore': 7, 'Delhi': 8, 'Chandigarh': 9, 'Kanpur': 10, 'Chennai': 11, 'Jaipur': 12, 'Visakhapatnam': 13, 'Abu Dhabi': 14, 'Dubai': 15, 'UAE': 16, 'Ahmedabad': 17, 'Sharjah': 18, 'Navi Mumbai': 19, 'Guwahati': 20, 'Cape Town': 21, 'Port Elizabeth': 22, 'Durban': 23, 'Centurion': 24, 'East London': 25, 'Johannesburg': 26, 'Kimberley': 27, 'Bloemfontein': 28, 'Cuttack': 29, 'Nagpur': 30, 'Dharamsala': 31, 'Raipur': 32, 'Ranchi': 33}
 
 st.title("ML Model Deployment with Streamlit")
 
